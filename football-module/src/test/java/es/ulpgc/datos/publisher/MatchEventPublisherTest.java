97800aa4b44ed4068539d0f7df0499af721804df
Gabriel Perez Doreste <gabriel.perez114@alu.ulpgc.es>
2026-04-20 11:08:53 +0100

    MatchEventPublisherTest for football-module

---
+++ football-module/src/test/java/es/ulpgc/datos/publisher/MatchEventPublisherTest.java
@@ -0,0 +1,27 @@
+package es.ulpgc.datos.publisher;
+
+import es.ulpgc.datos.model.Match;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchEventPublisherTest {
+
+    @Test
+    void publishDoesNotThrowWithEmptyList() {
+        MatchEventPublisher publisher = new MatchEventPublisher();
+        assertDoesNotThrow(() -> publisher.publish(List.of()));
+    }
+
+    @Test
+    void publishDoesNotThrowWithValidMatches() {
+        MatchEventPublisher publisher = new MatchEventPublisher();
+        Match match = new Match("Real Madrid CF", "FC Barcelona", 3, 2,
+                "FINISHED", "Primera Division", LocalDateTime.of(2026, 3, 22, 20, 0));
+
+        assertDoesNotThrow(() -> publisher.publish(List.of(match)));
+    }
+}
