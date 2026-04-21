4d9a0d8bacdff853d9f18278f8e66d8cab8dd8f4
Gabriel Perez Doreste <gabriel.perez114@alu.ulpgc.es>
2026-04-21 23:02:08 +0100

    changes on controller

--- football-module/src/main/java/es/ulpgc/datos/Controller.java
+++ football-module/src/main/java/es/ulpgc/datos/Controller.java
@@ -3,7 +3,7 @@ package es.ulpgc.datos;
 import es.ulpgc.datos.feeder.FootballFeeder;
 import es.ulpgc.datos.model.Match;
 import es.ulpgc.datos.publisher.MatchEventPublisher;
-import es.ulpgc.datos.storer.MatchStore;
+import es.ulpgc.datos.store.MatchStore;
 
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -13,12 +13,12 @@ import java.util.concurrent.TimeUnit;
 public class Controller {
 
     private final FootballFeeder feeder;
-    private final MatchStore serializer;
+    private final MatchStore store;
     private final MatchEventPublisher publisher;
 
-    public Controller(FootballFeeder feeder, MatchStore serializer, MatchEventPublisher publisher) {
+    public Controller(FootballFeeder feeder, MatchStore store, MatchEventPublisher publisher) {
         this.feeder = feeder;
-        this.serializer = serializer;
+        this.store = store;
         this.publisher = publisher;
     }
 
@@ -34,7 +34,7 @@ public class Controller {
         System.out.println("Partidos obtenidos: " + matches.size());
         matches.forEach(System.out::println);
         System.out.println("Guardando en base de datos...");
-        serializer.serialize(matches);
+        store.serialize(matches);
         System.out.println("Publicando eventos en ActiveMQ...");
         publisher.publish(matches);
     }
