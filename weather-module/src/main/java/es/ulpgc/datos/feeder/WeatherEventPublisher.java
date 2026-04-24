50509535b54cfa21783577592d6937609443654e
gael <gaelhb006@gmail.com>
2026-04-24 23:56:07 +0100

    Add WeatherEventPublisher to publish events to ActiveMQ

---
+++ weather-module/src/main/java/es/ulpgc/datos/feeder/WeatherEventPublisher.java
@@ -0,0 +1,40 @@
+package es.ulpgc.datos.feeder;
+
+import com.google.gson.Gson;
+import es.ulpgc.datos.model.WeatherEvent;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import jakarta.jms.*;
+
+public class WeatherEventPublisher {
+
+    private static final String BROKER_URL = "tcp://localhost:61616";
+    private static final String TOPIC_NAME = "Weather";
+
+    private final Gson gson = new Gson();
+
+    public void publish(WeatherEvent event) {
+        try {
+            ConnectionFactory factory = new ActiveMQConnectionFactory(BROKER_URL);
+            Connection connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createTopic(TOPIC_NAME);
+            MessageProducer producer = session.createProducer(destination);
+
+            String json = gson.toJson(event);
+            TextMessage message = session.createTextMessage(json);
+            producer.send(message);
+
+            System.out.println("Evento publicado: " + json);
+
+            producer.close();
+            session.close();
+            connection.close();
+
+        } catch (JMSException e) {
+            System.err.println("Error al publicar evento en ActiveMQ: " + e.getMessage());
+        }
+    }
+}
