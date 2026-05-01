59c25c3e63b3f5f80a0084776e9a35a5ed3bdfc8
Gabriel Perez Doreste <gabriel.perez114@alu.ulpgc.es>
2026-05-01 22:14:58 +0100

    correct cities of first division

--- weather-module/src/main/java/es/ulpgc/datos/feeder/OpenWeatherMapFeeder.java
+++ weather-module/src/main/java/es/ulpgc/datos/feeder/OpenWeatherMapFeeder.java
@@ -15,7 +15,6 @@ public class OpenWeatherMapFeeder implements WeatherFeeder {
 
     private static final List<String> CITIES = List.of(
             "Madrid", "Barcelona", "Seville", "Valencia", "Bilbao",
-            "Las Palmas de Gran Canaria", "Zaragoza", "Malaga",
             "Girona", "Pamplona", "Palma", "San Sebastian",
             "Villarreal", "Vigo", "Vitoria", "Elche", "Oviedo"
     );
